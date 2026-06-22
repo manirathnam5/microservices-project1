b41f4cab0a2ff4eecd49bcd12306ab0ea3e17ab6
manirathnam5 <manirathnam501@gmail.com>
2026-06-22 16:12:25 +0530

    rest template controller

--- interservice/consumerapp/src/main/java/com/microservice/consumerapp/controller/RestTemplateController.java
+++ interservice/consumerapp/src/main/java/com/microservice/consumerapp/controller/RestTemplateController.java
@@ -1,12 +1,32 @@
 package com.microservice.consumerapp.controller;
 
 
+import com.microservice.consumerapp.service.RestTemplateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/consumer-app")
-public class ConsumerController {
+@RequestMapping("/consumer-api/rest-template")
+@RequiredArgsConstructor
+public class RestTemplateController {
 
+    //fetch the details from producer controller which is producer
 
- }
+    @Autowired
+    public RestTemplateService restTemplateService;
+
+    @GetMapping("/instance-info")
+    public String getInstance() {
+
+        return restTemplateService.getInstanceInfo();
+
+    /*    RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://localhost:8080/producer-app/info", String.class);
+*/
+    }
+
+}
