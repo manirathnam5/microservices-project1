cd3ac16331fe6f007abfdfcd2c33f067e9bacd89
manirathnam5 <manirathnam501@gmail.com>
2026-06-24 22:24:25 +0530

    producer controller

--- interservice/producerapp/src/main/java/com/microservice/producerapp/controller/ProducerController.java
+++ interservice/producerapp/src/main/java/com/microservice/producerapp/controller/ProducerController.java
@@ -13,14 +13,14 @@ public class ProducerController {
     @Value("${server.port}")
     private String serverPort;
 
-    private Integer requestCount= 0;
+    private Integer requestCount = 0;
 
     private final String instanceId = java.util.UUID.randomUUID().toString();
 
 
     @GetMapping("/info")
     public String getProducerInfo() {
-        System.out.println( requestCount++ +" Request recieved at  producer app instance " + serverPort);
+        System.out.println(requestCount++ + " Request recieved at  producer app instance " + serverPort);
         return "Instance served by port :  " + serverPort + " Instance ID : " + instanceId;
 
     }
